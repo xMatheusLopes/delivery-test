001292304634af76ff60d0c50860f98fde9326de
Matheus Lopes <matheushl1996@gmail.com>
2024-07-25 23:11:36 -0300

    Angular Material setup

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,13 +1,8 @@
 import { http, HttpResponse } from 'msw'
+import deliveries from './data/deliveries.json';
  
 export const handlers = [
-  // Intercept "GET https://example.com/user" requests...
-  http.get('https://viacep.com.br/ws/01001000/json/', () => {
-    // ...and respond to them using this JSON response.
-    return HttpResponse.json({
-      id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
-      firstName: 'John',
-      lastName: 'Maverick',
-    })
+  http.get('/deliveries', () => {
+    return HttpResponse.json(deliveries)
   }),
 ]
